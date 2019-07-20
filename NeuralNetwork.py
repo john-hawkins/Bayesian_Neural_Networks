Fixing the problem with MASE calculation

NeuralNetwork.py
@@ -92,26 +92,26 @@ class NeuralNetwork:
     ######################################################################
     # RMSE - Root Mean Squared Error
     ######################################################################
-    def rmse(self, predictions, targets):
+    def rmse(self, predictions, targets, features):
         return np.sqrt(((predictions - targets) ** 2).mean())
 
     ######################################################################
     # Mean Absolute Error
     ######################################################################
-    def mae(self, predictions, targets):
+    def mae(self, predictions, targets, features):
         return (np.abs(predictions - targets)).mean()
 
     ######################################################################
     # Mean Absolute Percentage Error (with correction for zero target) 
     ######################################################################
-    def mape(self, predictions, targets):
+    def mape(self, predictions, targets, features):
         return (np.abs(predictions - targets)/(targets+0.0000001)).mean()
 
     ######################################################################
     # AUC - Area Under the Curve (Binary Classification Only)
     # TODO: Implement
     ######################################################################
-    def auc(self, predictions, targets):
+    def auc(self, predictions, targets, features):
         #joined = 
         #sorted = 
         return (np.abs(predictions - targets)/(targets+0.0000001)).mean()
@@ -126,7 +126,7 @@ class NeuralNetwork:
     # NOTE: Adding a small value to correct for instances when 
     #       the base error is zero 
     ######################################################################
-    def mase(self, predictions, targets):
+    def mase(self, predictions, targets, features):
         naive_preds = targets[0:len(targets)-1]
         final_targs = targets[1:len(targets)]
         model_preds = predictions[1:len(predictions)]
@@ -147,15 +147,13 @@ class NeuralNetwork:
     # TODO: In order to use this we need to change all definitions and calls
     #       of the eval function so that the features are also passed through
     ############################################################################
-    def maseb(self, features, predictions, targets):
-        naive_preds = features[len(features),:]
+    def maseb(self, predictions, targets, features):
+        naive_preds = features[:, features.shape[1]-1 ]
         final_targs = targets
         model_preds = predictions
         base_error = np.abs(naive_preds - final_targs) + 0.0000001
         model_error = np.abs(model_preds - final_targs)
-        return (model_error/base_error).mean()
-
-
+        return (model_error.sum()/base_error.sum())
 
 
     ######################################################################

analyse_test_results.py
@@ -30,6 +30,7 @@ from transform import Normalizer as nzr
 # - NAIVE_COL_NAME: CALCULATING MASE
 #
 #################################################################################
+
 def main():
     if len(sys.argv) < 6:
         print("ERROR: MISSING ARGUMENTS")
@@ -144,23 +145,23 @@ def summarise_model_calibration( test_data, target_name, results_path ):
 
 
 #################################################################################
-#  CALCULATE PERFORMANCE STATISTICS ABOUT THE MODEL
+#  CALCULATE PERFORMANCE STATISTICS OF THE MODEL
 #################################################################################
 def summarise_model_performance( test_data, target_name, results_path, naive_col='' ):
     df = test_data.copy()
     df["base_error"] =  df['mu'] - df[target_name]
-    df["nominal_target"] = np.where(df[target_name]==0,0.000001,df[target_name])
+    df["nominal_target"] = np.where(df[target_name]==0, 0.000001, df[target_name])
     df["abs_percent_error"] = abs(100*df["base_error"]/df["nominal_target"])
     df["abs_error"] =  abs(df["base_error"])
     df["naive_error"] =  df[naive_col] - df[target_name]
-    df["nominal_naive_error"] = np.where(df['naive_error']==0,0.000001,df['naive_error'])
-    df["abs_scaled_error"] = abs( df["base_error"] / df["nominal_naive_error"] )
-    df["abs_naive_error"] =  abs( df["nominal_naive_error"] )
+    df["abs_naive_error"] =  abs( df["naive_error"] )
+    df["abs_percent_error_naive"] = abs(100*df["naive_error"]/df["nominal_target"])
     sum_data = {
-                 'MAE': [df["abs_error"].mean() ],
-                 'MAPE': [df["abs_percent_error"].mean() ],
-                 'MASE': [df["abs_scaled_error"].mean() ],
-                 'MAE Naive': [df["abs_naive_error"].mean() ]
+                 'MAE': [ df["abs_error"].mean() ],
+                 'MAPE': [ df["abs_percent_error"].mean() ],
+                 'MASE': [ df["abs_error"].sum() / df["abs_naive_error"].sum() ],
+                 'MAE Naive': [ df["abs_naive_error"].mean() ],
+                 'MAPE Naive': [ df["abs_percent_error_naive"].mean() ]
                }
     sum_df = pd.DataFrame(sum_data)
     sum_df.to_csv(results_path + '/testdata_performance.csv', index=False)