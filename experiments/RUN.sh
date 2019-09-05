a052964d7124cc680a0a45654f93aee596bb0c7b
John Hawkins <john.hawkins@john.hawkins.MacBook>
2019-09-05 15:57:55 +1000

    Master RUN command

--- experiments/RUN.sh
+++ experiments/RUN.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-./RUN_BeiJing_24hr_LangevinFFNN.sh
-./RUN_BeiJing_24hr_SLP.sh
-./RUN_BeiJing_7day_LangevinFFNN.sh
-./RUN_BeiJing_FFNN.sh
 ./RUN_Delhi_FFNN.sh
 ./RUN_Delhi_LangevinFFNN.sh
 ./RUN_Delhi_SLP.sh
+
+./RUN_BeiJing_24hr_SLP.sh
+./RUN_BeiJing_24hr_FFNN.sh
+./RUN_BeiJing_24hr_LangevinFFNN.sh
+
+./RUN_BeiJing_7day_LangevinFFNN.sh
 
