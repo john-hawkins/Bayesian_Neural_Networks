723e5be001df5abacb41c77474614111c41e655a
John Hawkins <john.hawkins@john.hawkins.MacBook>
2019-09-05 16:01:50 +1000

    Forcing python3

--- experiments/RUN_BeiJing_24hr_FFNN.sh
+++ experiments/RUN_BeiJing_24hr_FFNN.sh
@@ -2,11 +2,11 @@
 
 cd ../
 
-python ./train_bn_mcmc.py 53 20 1 0 FFNN sigmoid data/Beijing/sets/Train_24_hour_norm.csv data/Beijing/sets/Test_24_hour_norm.csv results/Beijing_FFNN_24Hr_Sigmoid/  MASE 100000
+python3 ./train_bn_mcmc.py 53 20 1 0 FFNN sigmoid data/Beijing/sets/Train_24_hour_norm.csv data/Beijing/sets/Test_24_hour_norm.csv results/Beijing_FFNN_24Hr_Sigmoid/  MASE 100000
 
 # PROCESS THE RESULTS SO THAT THEY ARE IN THE TARGET SPACE (DEAL WITH NORMALISED OR DIFFERENCED TARGETS)
 
-python ./transform_test_results.py "./results/Beijing_FFNN_24Hr_Sigmoid/test_predictions_final.tsv" "./results/Beijing_FFNN_24Hr_Sigmoid/test_predictions.tsv" "./data/Beijing/sets/Test_24_hour_full.csv" "data/Beijing/sets/Target_24_nzr_config.yaml" True False False False "TARGET_pm2.5_24_VALUE" "pm2.5"
+python3 ./transform_test_results.py "./results/Beijing_FFNN_24Hr_Sigmoid/test_predictions_final.tsv" "./results/Beijing_FFNN_24Hr_Sigmoid/test_predictions.tsv" "./data/Beijing/sets/Test_24_hour_full.csv" "data/Beijing/sets/Target_24_nzr_config.yaml" True False False False "TARGET_pm2.5_24_VALUE" "pm2.5"
 
-python analyse_test_results.py "./results/Beijing_FFNN_24Hr_Sigmoid" "./results/Beijing_FFNN_24Hr_Sigmoid/test_predictions_final.tsv" "./data/Beijing/sets/Test_24_hour_full.csv" 50000 "TARGET_pm2.5_24_VALUE" "pm2.5"
+python3 analyse_test_results.py "./results/Beijing_FFNN_24Hr_Sigmoid" "./results/Beijing_FFNN_24Hr_Sigmoid/test_predictions_final.tsv" "./data/Beijing/sets/Test_24_hour_full.csv" 50000 "TARGET_pm2.5_24_VALUE" "pm2.5"
 
--- experiments/RUN_Delhi_FFNN.sh
+++ experiments/RUN_Delhi_FFNN.sh
@@ -4,10 +4,10 @@ cd ../
 
 # Delhi Air Quality
 
-python ./train_bn_mcmc.py 20 5 1 0 FFNN sigmoid data/Delhi/STN_144/train_normalised.csv  data/Delhi/STN_144/test_normalised.csv results/Delhi_144_FFNN_Sigmoid/ MASE 100000 
+python3 ./train_bn_mcmc.py 20 5 1 0 FFNN sigmoid data/Delhi/STN_144/train_normalised.csv  data/Delhi/STN_144/test_normalised.csv results/Delhi_144_FFNN_Sigmoid/ MASE 100000 
 
-python ./transform_test_results.py "./results/Delhi_144_FFNN_Sigmoid/test_predictions_final.tsv" "./results/Delhi_144_FFNN_Sigmoid/test_predictions.tsv" "./data/Delhi/STN_144/test.csv" "data/Delhi/STN_144/nzr_config.yaml" True False False False "TARGET_STN_144_PM10_7_VALUE" "STN_144_PM10"
+python3 ./transform_test_results.py "./results/Delhi_144_FFNN_Sigmoid/test_predictions_final.tsv" "./results/Delhi_144_FFNN_Sigmoid/test_predictions.tsv" "./data/Delhi/STN_144/test.csv" "data/Delhi/STN_144/nzr_config.yaml" True False False False "TARGET_STN_144_PM10_7_VALUE" "STN_144_PM10"
 
-python analyse_test_results.py "./results/Delhi_144_FFNN_Sigmoid" "./results/Delhi_144_FFNN_Sigmoid/test_predictions_final.tsv" "./data/Delhi/STN_144/test.csv" 50000 "TARGET_STN_144_PM10_7_VALUE" "STN_144_PM10"
+python3 analyse_test_results.py "./results/Delhi_144_FFNN_Sigmoid" "./results/Delhi_144_FFNN_Sigmoid/test_predictions_final.tsv" "./data/Delhi/STN_144/test.csv" 50000 "TARGET_STN_144_PM10_7_VALUE" "STN_144_PM10"
 
 
